5b0cc2b8b626e17d3ffc64c09cd53101777e2fff
MichalHalaj <79479385+MichalHalaj@users.noreply.github.com>
2022-04-04 09:28:57 +0200

    Add files via upload

---
+++ vikendovka/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+function showMeClassBejbe($trida)
+{
+    require("class/$trida.php");
+}
+
+spl_autoload_register("showMeClassBejbe");
+
+$ludia = array();
+$ludia[] = new Clovek('Michal','Halaj',27);
+$ludia[] = new Clovek('Samo','Rúfus',14);
+
+$ludia[] = new Javista('Jan','Mičuda',26,'VS code zatiaľ');
+$ludia[] = new Javista('Imrich','Zárudsky',35,'VS code zatiaľ');
+
+// Dedenie + polymorfizmus na funkciu pozdrav
+foreach($ludia as $clovek)
+{
+    $clovek->pozdrav();
+    echo'<br>';
+}
+
+// Hromadná genocída :D
+foreach($ludia as $chudaci)
+{
+    $chudaci->behaj(10);
+    $chudaci->behaj(10);
+    echo $chudaci->celeMeno() . $chudaci->behaj(10);
+    echo'<br>';
+}
