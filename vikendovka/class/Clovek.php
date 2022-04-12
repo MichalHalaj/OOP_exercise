e5e3bfdcf5c100ef9278c7fb7dd97bc5bad6c124
MichalHalaj <79479385+MichalHalaj@users.noreply.github.com>
2022-04-13 01:00:56 +0200

    Update Clovek.php

--- vikendovka/class/Clovek.php
+++ vikendovka/class/Clovek.php
@@ -1,21 +1,35 @@
 <?php 
+// utf kodovanie
+mb_internal_encoding("UTF-8");
 
-class Clovek
+class Clovek extends Bytost
 {
 
    
-    protected string $meno;
+    public string $meno;
     protected string $priezvisko;
-    protected int $vek;
+    public int $vek;
     private int $unava = 0;
+    public $id;
+    protected static $pocetLudi = 0;
+    private $heslo;
+    const MIN_PASS = 5;
+    const MAX_PASS = 13;
 
 
-    public function __construct($meno,$priezvisko,$vek)
+    public function __construct($meno,$priezvisko,$vek,$heslo)
     {
-        $this->meno = $meno;
+        parent::__construct($meno,$vek);
         $this->priezvisko = $priezvisko;  
-        $this->vek = $vek;
+        self::$pocetLudi++;
+        $this->id = self::$pocetLudi;
+        $this->heslo = $heslo;
     }
+
+
+
+
+
     
     public function celeMeno()
     {
@@ -24,7 +38,7 @@ class Clovek
 
     public function pozdrav()
     {
-        echo'Ahoj, ja som '. $this->meno;
+        echo'Ahoj, ja som '. $this->meno . ' | ID užívateľa: ' . $this->id;
     }
 
     public function spi($doba)
@@ -34,12 +48,22 @@ class Clovek
             $this->unava = 0;
     }
 
-    public function behaj($vzdalenost)
+    public function behaj($vzdialenost)
     {
-        if ($this->unava + $vzdalenost <= 20)
-            $this->unava += $vzdalenost;
+        if ($this->unava + $vzdialenost <= 20)
+            $this->unava += $vzdialenost;
         else
             echo('Som příliš unavený, 3tí maraton už nedám :( // ');
+    }
+
+    public static function validneHeslo($heslo)
+{
+    return (mb_strlen($heslo) >= self::MIN_PASS);
+}
+
+    public function __toString()
+    {
+        return $this->meno . ' - ' . $this->id;
     }
 
 }
